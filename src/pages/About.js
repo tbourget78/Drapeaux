edafb46db2d14dd3e7a599d41dfdc4b923760f70
Thomas Bourget <T.bourgetdev@outlook.fr>
2022-09-16 15:04:29 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum nulla
+        natus, ad eos ipsa nesciunt minus labore, ducimus dolore cumque quos
+        nihil quis in rem error ex, culpa cupiditate optio reiciendis. Aperiam,
+        cumque doloremque sequi unde perspiciatis dolorum qui odit quas iusto,
+        tenetur necessitatibus. Mollitia, fugit. Asperiores accusamus quasi
+        dolor quos delectus quaerat praesentium. Fugiat, odio neque omnis
+        tenetur dolorum nobis quis corporis sapiente dolor, recusandae enim,
+        velit sed eligendi. Veritatis, illum! Ex minus saepe omnis laborum
+        accusamus in sint unde, optio error eius ducimus excepturi cum magnam
+        sed maiores nesciunt, aliquid ullam inventore quas sapiente esse
+        recusandae ad dolor!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias non
+        facere voluptates fuga maiores! Tenetur deserunt facilis asperiores
+        impedit quae quaerat voluptate dolores! Animi sapiente deserunt odio aut
+        quas magni.
+      </p>
+    </div>
+  );
+};
+
+export default About;
